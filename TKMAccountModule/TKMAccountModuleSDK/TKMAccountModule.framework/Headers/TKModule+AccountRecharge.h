3253de7ac8c2a69119593abfb9a3132c855d7bd1
xianda.zheng <xianda.zheng@email.com>
2021-03-26 16:18:27 +0800

    0.1.2

---
+++ TKMAccountModule/TKMAccountModuleSDK/TKMAccountModule.framework/Headers/TKModule+AccountRecharge.h
@@ -0,0 +1,18 @@
+//
+//  TKModule+AccountRecharge.h
+//  TKMRechargeModule
+//
+//  Created by Thief Toki on 2021/3/26.
+//
+
+#import "TKModule.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TKModule (AccountRecharge)
+
+- (void)showRechargeView;
+
+@end
+
+NS_ASSUME_NONNULL_END
